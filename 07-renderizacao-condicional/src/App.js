063e341e11b892bd5dde426c78c86ee37e06bed7
iago-srm <iago_srm@yahoo.com.br>
2021-08-24 19:50:15 -0300

    correcoes de estrutura

--- 07-renderizacao-condicional/src/App.js
+++ 07-renderizacao-condicional/src/App.js
@@ -1,7 +1,7 @@
 function App() {
     return (
       <div>
-        <h1>Documentação React</h1>
+        <h1>Insira Docs aqui</h1>
       </div>
     );
   }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-docs
 Exemplos da Documentação do React
+
+Para adicionar um novo módulo, copiar a pasta 00-estrutura e inserir código ali.
