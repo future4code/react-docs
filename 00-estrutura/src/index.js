b9fa1015384bfc27465fd48450fdd046a1877476
BrunoMovio <bruno.labenu@gmail.com>
2021-09-08 21:23:03 -0300

    a

--- 00-estrutura/src/index.js
+++ 00-estrutura/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+<<<<<<< HEAD:00-estrutura/src/index.js
 import App from './app';
+=======
+import App from './App-componentizado';
+>>>>>>> add componentizacao e readmes:02-Componentizando/src/index.js
 
 ReactDOM.render(
   <React.StrictMode>
--- 02-Componentizando/src/index.js
+++ 02-Componentizando/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+<<<<<<< HEAD
 import App from './App-componentizado';
+=======
+<<<<<<< HEAD:00-estrutura/src/index.js
+import App from './app';
+=======
+import App from './App-componentizado';
+>>>>>>> add componentizacao e readmes:02-Componentizando/src/index.js
+>>>>>>> add componentizacao e readmes
 
 ReactDOM.render(
   <React.StrictMode>
