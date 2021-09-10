a8fbc0ff5625b9ea7511fd7d30e5a9c79d124180
camila monteiro <monteirocamis@gmail.com>
2021-09-09 22:06:23 -0300

    :construction: primeiro exemplo de intro jsx

--- 01-intro-JSX/src/index.js
+++ 01-intro-JSX/src/index.js
@@ -2,29 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import jsx1 from './images/jsx1.png'
 
 const elemento = 'Incorporando Expressões em JSX  !';
 const tituloDaPagina = <h1>Olá, vamos falar de  {elemento}</h1>;
+
 const elemento2 = (
-  <div>
-    <p> 
+
+    <div>
+   
+        <p> 
+          Neste exemplo , declaramos uma variável chamada elemento2 e então a 
+          usamos dentro do JSX ao envolvê-la com chaves:
+        </p>;
 
 
-Neste exemplo , declaramos uma variável chamada elemento2 e então a 
-usamos dentro do JSX ao envolvê-la com chaves:
-</p>;
-
-<img src={jsx1} alt="codigo em imagem"> </img>
-
-  </div>
-
-);
+  
+    </div>
+  );
 
 
 ReactDOM.render(
-
- elemento2,
+//elemento,
+elemento2,
   document.getElementById('root')
 );
