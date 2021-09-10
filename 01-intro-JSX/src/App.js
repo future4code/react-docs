a7c6ffa2c119198967d66d275bb426f2d17fc7ba
camila monteiro <monteirocamis@gmail.com>
2021-09-09 21:20:59 -0300

    :construction: construindo pagina jsx

---
+++ 01-intro-JSX/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<div>
+<p> 
+      Incorporando Expressões em JSX
+
+No exemplo abaixo, declaramos uma variável chamada name e então a usamos dentro do JSX ao envolvê-la com chaves:
+      </p>
+</div>
+ 
+    </div>
+  );
+}
+
+export default App;
--- 01-intro-JSX/src/index.js
+++ 01-intro-JSX/src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-<<<<<<< HEAD:00-estrutura/src/index.js
-import App from './app';
-=======
-import App from './App-componentizado';
->>>>>>> add componentizacao e readmes:02-Componentizando/src/index.js
+const elemento = 'Intro JSX !';
+const tituloDaPagina = <h1>Olá, vamos falar de  {elemento}</h1>;
+
+
+// <div>
+//     <h1>Hello!</h1>
+//     <h2>Good to see you here.</h2>
+//   </div>
+
+const elemento2 = (
+  <div>
+    <p> 
+Incorporando Expressões em JSX
+
+No exemplo abaixo, declaramos uma variável chamada name e então a usamos dentro do JSX ao envolvê-la com chaves:
+</p>;
+  </div>
+);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// tituloDaPagina,
+elemento,
+  //  elemento2,
   document.getElementById('root')
 );
